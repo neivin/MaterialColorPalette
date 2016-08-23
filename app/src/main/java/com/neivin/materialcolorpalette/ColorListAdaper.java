16c8f5c80256bea1ab2d6fb5dfa8828f8a6dde0a
neivin <neivin93@gmail.com>
2016-08-23 02:59:54 -0400

    Improved readability on light backgrounds

--- app/src/main/java/com/neivin/materialcolorpalette/ColorListAdaper.java
+++ app/src/main/java/com/neivin/materialcolorpalette/ColorListAdaper.java
@@ -29,13 +29,45 @@ public class ColorListAdaper extends ArrayAdapter<ColorValue> {
 
         ColorValue color = getItem(position);
 
+        String hexCode = color.getHexCode();
+
         TextView colorGradeView = (TextView) listItemView.findViewById(R.id.color_grade);
         colorGradeView.setText(color.getColorGrade());
 
         TextView colorHexCodeView = (TextView) listItemView.findViewById(R.id.color_hex_code);
-        colorHexCodeView.setText(color.getHexCode());
+        colorHexCodeView.setText(hexCode);
 
+        // Set the background color
         listItemView.setBackgroundColor(Color.parseColor(color.getHexCode()));
+
+
+        /* How to tell if the color is light or dark from its hex value.
+         * Credit to Dunes on StackOverflow
+         */
+
+        // remove hash character from string
+        String rawFontColor = hexCode.substring(1,hexCode.length());
+
+        // convert hex string to int
+        int rgb = Integer.parseInt(rawFontColor, 16);
+
+        float [] hsv = new float[3];
+
+        Color.RGBToHSV(Color.red(rgb), Color.green(rgb), Color.blue(rgb), hsv);
+
+        float brightness = hsv[2];
+
+        if (brightness <= 0.8) {
+            // use white text
+            colorGradeView.setTextColor(Color.WHITE);
+            colorHexCodeView.setTextColor(Color.WHITE);
+        } else {
+            // use dark text
+            colorGradeView.setTextColor(Color.BLACK);
+            colorHexCodeView.setTextColor(Color.BLACK);
+        }
+
+
         return listItemView;
     }
 }
