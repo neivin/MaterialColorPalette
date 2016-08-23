364ab58be79ea976befe190b24b3fa42070c6f38
neivin <neivin93@gmail.com>
2016-08-23 01:42:34 -0400

    Improved readability of grid items.

--- app/src/main/java/com/neivin/materialcolorpalette/ColorGridAdapter.java
+++ app/src/main/java/com/neivin/materialcolorpalette/ColorGridAdapter.java
@@ -27,21 +27,29 @@ public class ColorGridAdapter extends ArrayAdapter<String> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View gridItemView = convertView;
-        if(gridItemView == null){
-            gridItemView = LayoutInflater.from(getContext()).inflate(R.layout.grid_item_layout,parent,false);
+        if (gridItemView == null) {
+            gridItemView = LayoutInflater.from(getContext()).inflate(R.layout.grid_item_layout, parent, false);
         }
 
         String colorName = getItem(position);
 
         TextView colorNameTextView = (TextView) gridItemView.findViewById(R.id.color_name);
         colorNameTextView.setText(colorName);
-        colorNameTextView.setTextColor(Color.WHITE);
 
+        if (colorName.contains("Light") || colorName.equals("Grey") || colorName.equals("Yellow") || colorName.equals("Amber")
+                || colorName.equals("Orange") || colorName.equals("Green")
+                || colorName.equals("Lime") || colorName.equals("Cyan")) {
+            colorNameTextView.setTextColor(Color.BLACK);
+        }
+        else{
+            colorNameTextView.setTextColor(Color.WHITE);
+        }
 
 
         String resourceName = colorName.toLowerCase().replace(' ', '_') + "_500";
-        Log.i("Adapter", "resorceName: "+resourceName);
-        gridItemView.setBackgroundColor(ContextCompat.getColor(getContext(),getContext().getResources().getIdentifier(resourceName, "color", "com.neivin.materialcolorpalette" )));
+        Log.i("Adapter", "colorName: " + colorName);
+        gridItemView.setBackgroundColor(ContextCompat.getColor(getContext(), getContext().getResources().getIdentifier(resourceName, "color", "com.neivin.materialcolorpalette")));
+
 
         return gridItemView;
     }
